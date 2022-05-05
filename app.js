ab048cd6ab9b3aa3c1f527fc423557e211500f83
kpilszak <klaudia.pilszak@gmail.com>
2022-05-05 18:41:34 +0200

    Click bomb - game over

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let bombAmount = 20
   let squares = []
 
-  function createBoard() {
+  function createBoard () {
     // Get shuffled game array with random bombs
     const bombsArray = Array(bombAmount).fill('bomb')
     const emptyArray = Array(width * width - bombAmount).fill('valid')
@@ -17,7 +17,38 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffledArray[i])
       grid.appendChild(square)
       squares.push(square)
+
+      // Normal click
+      square.addEventListener('click', function (e) {
+        click(square)
+      })
+    }
+
+    // Add numbers
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0
+      const isLeftEdge = (i % width === 0)
+      const isRightEdge = (i % width === width - 1)
+
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
+        if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++
+        if (i > 10 && squares[i - width].classList.contains('bomb')) total++
+        if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+        if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++
+        if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+        if (i < 89 && squares[i + width].classList.contains('bomb')) total++
+        squares[i].setAttribute('data', total)
+      }
     }
   }
   createBoard()
+
+  // Click on square ations
+  function click (square) {
+    if (square.classList.contains('bomb')) {
+      console.log('Game over')
+    }
+  }
 })
